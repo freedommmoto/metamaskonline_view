1aec0cfd4629a41ea73526b1c8b23ab6bb42720f
Patara.m <patara.m@cartrack.com>
2022-07-16 23:10:53 +0700

    done add subscribe pusher for get line validation code and make alert on ui.

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,30 @@
 </template>
 
 <script>
+import Pusher from 'pusher-js' // import Pusher
+import { config } from '../config';
+import swal from 'sweetalert';
 
 export default {
   name: 'App',
-
+  created () {
+    this.subscribeCode()
+  },
+  methods:{
+    subscribeCode () {
+      console.log('in subscribe')
+      let pusher = new Pusher(config.$pusher_key, { cluster: 'ap1' })
+      pusher.subscribe('my-channel')
+      pusher.bind('my_event', data => {
+        let text = "You line code is "+data.message
+        swal(text,
+            "please enter this code in line metamaskonline group" +
+            " for validation and active you account.");
+        console.log(data)
+        //this.code = data.message
+      })
+    }
+  },
   components: {
     
   }
